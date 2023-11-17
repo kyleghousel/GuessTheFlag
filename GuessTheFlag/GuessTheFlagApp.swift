e10ae2996308cc1f2ce31263f54cef75f6f42b55
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-11-17 06:52:50 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kyle Housel on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
